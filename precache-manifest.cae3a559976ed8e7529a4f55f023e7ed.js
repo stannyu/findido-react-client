62c40fc54af5d4c9756c2eb55756baa6c08e3109
standman <stanislau.yudzitski@mendix.com>
2019-04-15 11:29:03 +0200

    Updates

---
+++ precache-manifest.cae3a559976ed8e7529a4f55f023e7ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2787c17f959f96be7848",
+    "url": "/findido-react-client/static/css/main.b63f9ad4.chunk.css"
+  },
+  {
+    "revision": "2787c17f959f96be7848",
+    "url": "/findido-react-client/static/js/main.a9de3392.chunk.js"
+  },
+  {
+    "revision": "73168e6aefc9d0f257c6",
+    "url": "/findido-react-client/static/js/runtime~main.ec0675c1.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/findido-react-client/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/findido-react-client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1b25a88646136159fd66401b14f4b56",
+    "url": "/findido-react-client/index.html"
+  }
+];
