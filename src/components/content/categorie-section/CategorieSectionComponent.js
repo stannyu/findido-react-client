40c065ac1e7d7b7c1ff6ad2a1d886ca7d6f8b062
standman <stanislau.yudzitski@mendix.com>
2019-04-28 20:16:49 +0200

    Universal categories section created and mapped to data.

--- src/components/content/categorie-section/CategorieSectionComponent.js
+++ src/components/content/categorie-section/CategorieSectionComponent.js
@@ -1,13 +1,51 @@
 import React, { Component } from "react";
 import "./CategorieSectionComponent.scss";
+
+import { getEntityByUrl } from "../../../http/http-wrapper";
+import { heatToUpper } from "../../../shared/helpers";
 import ArrRightSvgIcon from "./ArrRightSvgIcon";
 
+import CategorieCardComponent from "./categorie-card/CategorieCardComponent";
+
+const CATEGORIE_PL = "categories";
+
 class CategorieSectionComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categorieData: {}
+    };
+
+    this.setCategoriesDataToState = this.setCategoriesDataToState.bind(this);
+  }
+
+  async componentDidMount() {
+    await getEntityByUrl(
+      CATEGORIE_PL + "?" + this.props.categorie,
+      this.setCategoriesDataToState
+    );
+  }
+
+  setCategoriesDataToState(data) {
+    this.setState({
+      categorieData: data
+    });
+  }
+
   render() {
+    const categorieData = this.state.categorieData[this.props.categorie];
+
+    const CategoriesCards = categorieData
+      ? categorieData.map(categorie => (
+          <CategorieCardComponent categorie={categorie} key={categorie.id} />
+        ))
+      : [];
+
     return (
       <div className="categorie_section_wrapper">
         <div className="title_section_container">
-          <h3 className="title_section_container__title">#Food</h3>
+          <h3 className="title_section_container__title">#{heatToUpper(this.props.categorie)}</h3>
           <span className="title_section_container__link_all">
             View all
             <div className="arrow_right">
@@ -15,13 +53,8 @@ class CategorieSectionComponent extends Component {
             </div>
           </span>
         </div>
-        
-        <div className="categories_cards_container">
-          <div className="categorie_card"></div>
-          <div className="categorie_card"></div>
-          <div className="categorie_card"></div>
-          <div className="categorie_card"></div>
-        </div>
+
+        <div className="categories_cards_container">{CategoriesCards}</div>
       </div>
     );
   }
